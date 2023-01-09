f8955ec3bcdc35ad7814a55d3e915988411c7f09
fawemimo <f.owolabi81@gmail.com>
2023-01-09 11:56:26 +0100

    fixtures added

---
+++ pages/management/commands/load-fixtures.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        call_command("makemigrations")  
+        call_command("migrate")
+        call_command("loaddata", "db_componentdump.json")
+        call_command("loaddata", "db_mainbanner.json")
+        call_command("loaddata", "db_navlink.json")
+        call_command("loaddata", "db_navlinkitem.json")
+        call_command("loaddata", "db_sectionbanner.json")
+        call_command("loaddata", "db_seo_optimization.json")
+        call_command("loaddata", "db_testimonial.json")
+        call_command("loaddata", "db_topbar.json")
