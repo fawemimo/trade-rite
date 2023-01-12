1ab1cba25cdd1e40d1633f2bd9b315514e3b4cb4
fawemimo <f.owolabi81@gmail.com>
2023-01-12 16:24:30 +0100

    debug 404

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['localhost','trade-rite.ng', 'www.trade-rite.ng']
