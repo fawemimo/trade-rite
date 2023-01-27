c622b3d7cd39145162f2637fba161831a06493a8
fawemimo <f.owolabi81@gmail.com>
2023-01-27 20:42:39 +0100

    sites map removed

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,6 @@ ALLOWED_HOSTS = ['localhost','trade-rite.ng', 'www.trade-rite.ng','127.0.0.1']
 INSTALLED_APPS = [
     'django.contrib.admin', 
     'django.contrib.auth',
-    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
